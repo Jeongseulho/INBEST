a6f2ddc5d9b9475dbc078394fe25e4040defc09a
Hc-5514 <tbd05050@naver.com>
2023-09-04 18:05:55 +0900

    feat : OAuth2AuthenticationFailureHandler 추가
    소셜 로그인 실패시 작동할 FailureHandler 추가
    뼈대 작성 (로직 미구현)
    
    20230904 이호찬

---
+++ spring/src/main/java/com/jrjr/inbest/oauth/handler/OAuth2AuthenticationFailureHandler.java
@@ -0,0 +1,24 @@
+package com.jrjr.inbest.oauth.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) throws IOException, ServletException {
+		log.info("OAuth2AuthenticationFailureHandler 실행 - OAuth2 인증 실패");
+		super.onAuthenticationFailure(request, response, exception);
+	}
+}
