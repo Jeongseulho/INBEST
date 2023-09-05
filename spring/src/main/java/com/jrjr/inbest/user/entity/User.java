d9e905697e42b0fa286126a2811f5fe293e2f84f
Hc-5514 <tbd05050@naver.com>
2023-09-05 17:54:44 +0900

    refact : User Entity 수정 (provider column 제거)
    provider column 제거 (Login Entity 로 이동)
    
    20230905 이호찬

--- spring/src/main/java/com/jrjr/inbest/user/entity/User.java
+++ spring/src/main/java/com/jrjr/inbest/user/entity/User.java
@@ -44,21 +44,15 @@ public class User extends BaseEntity {
 	@Column(nullable = false)
 	private Integer gender;
 
-	@ColumnDefault("'inbest'")
-	@Column(nullable = false)
-	private String provider;
-
 	private LocalDateTime deletedDate;
 
 	@Builder
-	public User(String email, String name, String nickname, String birthyear, String birthday, Integer gender,
-		String provider) {
+	public User(String email, String name, String nickname, String birthyear, String birthday, Integer gender) {
 		this.email = email;
 		this.name = name;
 		this.nickname = nickname;
 		this.birthyear = birthyear;
 		this.birthday = birthday;
 		this.gender = gender;
-		this.provider = provider;
 	}
 }
