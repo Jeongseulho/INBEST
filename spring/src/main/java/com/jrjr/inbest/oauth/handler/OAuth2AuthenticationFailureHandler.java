9a7fb0384db988e1cc82a9c3bf843be465d76682
Hc-5514 <tbd05050@naver.com>
2023-09-05 16:23:16 +0900

    feat : OAuth2AuthenticationFailureHandler 수정
    다른 소셜 사이트에서 회원가입을 했을 경우, 401 (로그인 실패, 접근 거부)
    
    20230905 이호찬

--- spring/src/main/java/com/jrjr/inbest/oauth/handler/OAuth2AuthenticationFailureHandler.java
+++ spring/src/main/java/com/jrjr/inbest/oauth/handler/OAuth2AuthenticationFailureHandler.java
@@ -3,6 +3,7 @@ package com.jrjr.inbest.oauth.handler;
 import java.io.IOException;
 
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,9 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
 		log.info("OAuth2AuthenticationFailureHandler 실행 - OAuth2 인증 실패");
+		if (exception instanceof OAuth2AuthenticationException) {
+			log.info("getError: {}", ((OAuth2AuthenticationException)exception).getError());
+		}
 		super.onAuthenticationFailure(request, response, exception);
 	}
 }
