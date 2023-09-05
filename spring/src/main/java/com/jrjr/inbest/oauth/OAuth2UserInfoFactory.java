734dbc734ae8535f7086c914010e64f0c2eb711a
Hc-5514 <tbd05050@naver.com>
2023-09-05 15:42:00 +0900

    feat : OAuth2UserInfoFactory 수정
    attributes 내부의 Map 값들을 조회하기 위한 key 값 추가
    
    20230905 이호찬

--- spring/src/main/java/com/jrjr/inbest/oauth/OAuth2UserInfoFactory.java
+++ spring/src/main/java/com/jrjr/inbest/oauth/OAuth2UserInfoFactory.java
@@ -6,8 +6,8 @@ public class OAuth2UserInfoFactory {
 
 	public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
 		return switch (registrationId) {
-			case "NAVER" -> new NaverOAuth2UserInfo(attributes);
-			case "KAKAO" -> new KakaoOAuth2UserInfo(attributes);
+			case "NAVER" -> new NaverOAuth2UserInfo(attributes, "response");
+			case "KAKAO" -> new KakaoOAuth2UserInfo(attributes, "kakao_account");
 			default -> throw new IllegalArgumentException("Invalid Provider Type.");
 		};
 	}
