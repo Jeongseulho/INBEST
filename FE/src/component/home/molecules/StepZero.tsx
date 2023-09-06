98cd4e52b4f9041ee6c3636cf89ca36d30377028
Jeong seulho <wjdtmfgh@gmail.com>
2023-09-07 00:10:35 +0900

    fix : home image 스크롤 애니메이션 sticky 제거
    20230907 seulHo

--- FE/src/component/home/molecules/StepZero.tsx
+++ FE/src/component/home/molecules/StepZero.tsx
@@ -6,13 +6,7 @@ import { useStep } from "./useStep";
 const StepZero = ({ setCurStep }: StepProps) => {
   const { ref, animation, variants } = useStep(0, setCurStep);
   return (
-    <motion.div
-      ref={ref}
-      initial="hidden"
-      animate={animation}
-      variants={variants}
-      className=" h-screen sticky top-40 z-50"
-    >
+    <motion.div ref={ref} initial="hidden" animate={animation} variants={variants} className=" h-screen top-40 z-50">
       <img src={temp_logo} alt="" className=" w-1/2 h-1/2" />
     </motion.div>
   );
