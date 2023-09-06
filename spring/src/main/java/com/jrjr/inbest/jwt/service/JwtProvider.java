efe4463696092c74a200b30819685c7745e2b08e
Hc-5514 <tbd05050@naver.com>
2023-09-06 18:05:06 +0900

    feat : JwtProvider 로직 추가
    AccessToken 생성 로직 추가
    RefreshToken 생성 및 Redis 저장 로직 추가
    
    20230906 이호찬

--- spring/src/main/java/com/jrjr/inbest/jwt/service/JwtProvider.java
+++ spring/src/main/java/com/jrjr/inbest/jwt/service/JwtProvider.java
@@ -9,16 +9,22 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.jrjr.inbest.jwt.TokenExpireTime;
 import com.jrjr.inbest.jwt.dto.AccessTokenDto;
+import com.jrjr.inbest.jwt.entity.RefreshToken;
+import com.jrjr.inbest.jwt.repository.RefreshTokenRepository;
 import com.jrjr.inbest.login.constant.Role;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class JwtProvider {
 
-	@Value("${oauth.jwt.secret}")
+	@Value("${spring.oauth.jwt.secret}")
 	private String secretKey;
+
+	private final RefreshTokenRepository refreshTokenRepository;
 
 	private static final String GRANT_TYPE = "Bearer";
 
@@ -32,9 +38,32 @@ public class JwtProvider {
 			.withClaim("role", role.toString())
 			.sign(Algorithm.HMAC256(secretKey.getBytes()));
 
+		log.info("AccessToken 생성 완료: {}", accessToken);
+
 		return AccessTokenDto.builder()
 			.grantType(GRANT_TYPE)
 			.accessToken(accessToken)
 			.build();
+	}
+
+	public String generateRefreshToken(String email) {
+		log.info("JwtProvider - generateRefreshToken 실행");
+
+		String refreshToken = JWT.create()
+			.withIssuer("inbest")
+			.withSubject(email)
+			.withExpiresAt(new Date(System.currentTimeMillis() + TokenExpireTime.REFRESH_TOKEN_EXPIRE_TIME))
+			.sign(Algorithm.HMAC256(secretKey.getBytes()));
+
+		log.info("RefreshToken 생성 완료: {}", refreshToken);
+
+		RefreshToken refreshTokenEntity = refreshTokenRepository.save(RefreshToken.builder()
+			.email(email)
+			.refreshToken(refreshToken)
+			.build());
+
+		log.info("RefreshToken Redis 저장 완료: {}", refreshTokenEntity.getRefreshToken());
+
+		return refreshToken;
 	}
 }
