37e44c0b31fac5d0b2fa090616bf5930d1cda477
Jeong seulho <wjdtmfgh@gmail.com>
2023-09-06 10:13:56 +0900

    design : 전역 테마 색 지정

--- FE/tailwind.config.cjs
+++ FE/tailwind.config.cjs
@@ -13,6 +13,12 @@ module.exports = {
         bold: ["bold"],
         extraBold: ["extraBold"],
       },
+      colors: {
+        main: "#6CE061",
+        light: "#6FFF8C",
+        lima: "#ADF777",
+        lime: "#C1E061",
+      },
     },
   },
   plugins: [],
