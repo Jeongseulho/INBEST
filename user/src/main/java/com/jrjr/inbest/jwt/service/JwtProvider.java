4a12f8689765d2e6f124146e06f21c6d56cabb0d
Hc-5514 <tbd05050@naver.com>
2023-09-07 18:48:53 +0900

    feat : JwtProvider 로직 추가 및 변경
    1. AccessToken, RefreshToken 발급 관련 메서드 변경
    2. 토큰 검증 로직 추가
    3. claim 추출 로직 추가
    4. 현재 들고 있는 RefreshToken 과 저장된 RefreshToken 비교 로직 추가
    5. token 의 subject 를 이용해 email 과 role 반환하는 로직 추가
    
    20230907 이호찬

--- user/src/main/java/com/jrjr/inbest/jwt/service/JwtProvider.java
+++ user/src/main/java/com/jrjr/inbest/jwt/service/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.jrjr.inbest.jwt.service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.Optional;
 
@@ -7,14 +8,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.jrjr.inbest.jwt.TokenExpireTime;
 import com.jrjr.inbest.jwt.dto.AccessTokenDto;
 import com.jrjr.inbest.jwt.entity.RefreshToken;
 import com.jrjr.inbest.jwt.repository.RefreshTokenRepository;
 import com.jrjr.inbest.login.constant.Role;
+import com.jrjr.inbest.login.dto.LoginDto;
+import com.jrjr.inbest.login.entity.Login;
+import com.jrjr.inbest.login.repository.LoginRepository;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,18 +35,20 @@ public class JwtProvider {
 	private String secretKey;
 
 	private final RefreshTokenRepository refreshTokenRepository;
+	private final LoginRepository loginRepository;
 	private static final String HEADER_AUTHORIZATION = "Authorization";
 	private static final String GRANT_TYPE = "Bearer";
 
 	public AccessTokenDto generateAccessToken(String email, Role role) {
 		log.info("JwtProvider - generateAccessToken 실행");
 
-		String accessToken = JWT.create()
-			.withIssuer("inbest")
-			.withSubject(email)
-			.withExpiresAt(new Date(System.currentTimeMillis() + TokenExpireTime.ACCESS_TOKEN_EXPIRE_TIME))
-			.withClaim("role", role.toString())
-			.sign(Algorithm.HMAC256(secretKey.getBytes()));
+		String accessToken = Jwts.builder()
+			.setIssuer("inbest")
+			.setSubject(email)
+			.setExpiration(new Date(System.currentTimeMillis() + TokenExpireTime.ACCESS_TOKEN_EXPIRE_TIME))
+			.claim("role", role.toString())
+			.signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+			.compact();
 
 		log.info("AccessToken 생성 완료: {}", accessToken);
 
@@ -52,11 +61,12 @@ public class JwtProvider {
 	public String generateRefreshToken(String email) {
 		log.info("JwtProvider - generateRefreshToken 실행");
 
-		String refreshToken = JWT.create()
-			.withIssuer("inbest")
-			.withSubject(email)
-			.withExpiresAt(new Date(System.currentTimeMillis() + TokenExpireTime.REFRESH_TOKEN_EXPIRE_TIME))
-			.sign(Algorithm.HMAC256(secretKey.getBytes()));
+		String refreshToken = Jwts.builder()
+			.setIssuer("inbest")
+			.setSubject(email)
+			.setExpiration(new Date(System.currentTimeMillis() + TokenExpireTime.REFRESH_TOKEN_EXPIRE_TIME))
+			.signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+			.compact();
 
 		log.info("RefreshToken 생성 완료: {}", refreshToken);
 
@@ -81,8 +91,57 @@ public class JwtProvider {
 		return Optional.empty();
 	}
 
-	public Boolean isValidToken(String token) {
+	public boolean isValidToken(String token) {
 		log.info("JwtProvider - isValidToken 실행");
-		return false;
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+				.build()
+				.parseClaimsJws(token);
+			return true;
+		} catch (ExpiredJwtException e) {
+			log.info("EXPIRED_TOKEN", e);
+			return false;
+		} catch (Exception e) {
+			log.info("INVALID_TOKEN", e);
+			throw new JwtException("INVALID_TOKEN");
+		}
+	}
+
+	public Claims getClaims(String token) {
+		log.info("JwtProvider - getClaims 실행");
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		} catch (Exception e) {
+			log.info("INVALID_TOKEN", e);
+			throw new JwtException("INVALID_TOKEN");
+		}
+	}
+
+	public boolean compareRefreshTokens(String refreshToken) {
+		log.info("JwtProvider - compareRefreshTokens 실행");
+		Claims claims = this.getClaims(refreshToken);
+
+		RefreshToken refreshTokenEntity = refreshTokenRepository.findById(claims.getSubject())
+			.orElseThrow(() -> new JwtException("EXPIRED_REFRESH_TOKEN"));
+
+		return refreshToken.equals(refreshTokenEntity.getEmail());
+	}
+
+	public LoginDto getUserInfoFromToken(String token) {
+		log.info("JwtProvider - getUserInfoFromToken 실행");
+		Claims claims = this.getClaims(token);
+
+		Login loginEntity = loginRepository.findByEmail(claims.getSubject())
+			.orElseThrow(() -> new JwtException("INVALID_TOKEN"));
+
+		return LoginDto.builder()
+			.email(loginEntity.getEmail())
+			.role(loginEntity.getRole())
+			.build();
 	}
 }
