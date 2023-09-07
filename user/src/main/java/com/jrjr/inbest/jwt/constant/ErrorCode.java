ed4f075bb28247b79d1c167ff63cf355235c59b8
Hc-5514 <tbd05050@naver.com>
2023-09-07 18:41:50 +0900

    feat : ErrorCode enum 값 추가 (토큰 손상, AccessToken 재발급)
    INVALID_TOKEN, REISSUE_ACCESS_TOKEN 추가
    
    20230907 이호찬

--- user/src/main/java/com/jrjr/inbest/jwt/constant/ErrorCode.java
+++ user/src/main/java/com/jrjr/inbest/jwt/constant/ErrorCode.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+	INVALID_TOKEN("false", "INVALID_TOKEN", 401),
+	REISSUE_ACCESS_TOKEN("false", "REISSUE_ACCESS_TOKEN", 401),
 	EXPIRED_REFRESH_TOKEN("false", "EXPIRED_REFRESH_TOKEN", 401),
 	INTERNAL_SERVER_ERROR("false", "INTERNAL_SERVER_ERROR", 500);
 
