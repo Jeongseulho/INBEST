609717d6655d19b9519a2ee60bb9715037b49249
Hc-5514 <tbd05050@naver.com>
2023-09-07 14:52:34 +0900

    feat : JwtAccessDeniedHandler 추가 (초기 파일)
    JwtAccessDeniedHandler 초기 파일 생성
    
    20230907 이호찬

---
+++ spring/src/main/java/com/jrjr/inbest/jwt/handler/JwtAccessDeniedHandler.java
@@ -0,0 +1,21 @@
+package com.jrjr.inbest.jwt.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response,
+		AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+	}
+}
