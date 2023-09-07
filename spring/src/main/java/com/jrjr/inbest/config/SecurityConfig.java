0b20522167e23ecd7207375813776be9813d0faf
Hc-5514 <tbd05050@naver.com>
2023-09-07 11:50:20 +0900

    feat : SecurityConfig (Cors, JWT 설정)
    CorsFilter 및 JWT 관련 filter, handler 추가
    
    20230907 이호찬

--- spring/src/main/java/com/jrjr/inbest/config/SecurityConfig.java
+++ spring/src/main/java/com/jrjr/inbest/config/SecurityConfig.java
@@ -9,7 +9,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.filter.CorsFilter;
 
+import com.jrjr.inbest.jwt.filter.JwtAuthenticationFilter;
+import com.jrjr.inbest.jwt.filter.JwtExceptionFilter;
+import com.jrjr.inbest.jwt.handler.JwtAccessDeniedHandler;
+import com.jrjr.inbest.jwt.service.JwtProvider;
 import com.jrjr.inbest.oauth.handler.OAuth2AuthenticationFailureHandler;
 import com.jrjr.inbest.oauth.handler.OAuth2AuthenticationSuccessHandler;
 import com.jrjr.inbest.oauth.service.OAuth2UserService;
@@ -23,6 +30,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SecurityConfig {
 
+	private final CorsFilter corsFilter;
+	private final JwtExceptionFilter jwtExceptionFilter;
+	private final JwtProvider jwtProvider;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 	private final OAuth2UserService oAuth2UserService;
 	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 	private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
@@ -32,15 +43,25 @@ public class SecurityConfig {
 		http
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement((sessionManagement) ->
-				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable);
 
 		http.authorizeHttpRequests((authorizeHttpRequests) ->
 			authorizeHttpRequests
 				.anyRequest().permitAll());
 
-		http.logout((httpSecurityLogoutConfigurer ->
+		http.logout(httpSecurityLogoutConfigurer ->
 			httpSecurityLogoutConfigurer.logoutSuccessUrl("/login")
-		));
+		);
+
+		http.addFilterBefore(corsFilter, LogoutFilter.class)
+			.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class)
+			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+		http.exceptionHandling(
+			httpSecurityExceptionHandlingConfigurer -> httpSecurityExceptionHandlingConfigurer.accessDeniedHandler(
+				jwtAccessDeniedHandler));
 
 		http.oauth2Login((oAuth2LoginConfigurer) ->
 			oAuth2LoginConfigurer
