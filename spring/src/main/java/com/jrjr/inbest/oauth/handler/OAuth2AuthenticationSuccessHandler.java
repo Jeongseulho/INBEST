ff25f3a40b651572eee0bb896c2f7f4a711b076a
Hc-5514 <tbd05050@naver.com>
2023-09-07 10:40:32 +0900

    feat : onAuthenticationSuccess 수정 (AccessToken response header 추가)
    1. log 수정
    2. response header 에 AccessToken 반환
    
    20230907 이호찬

--- spring/src/main/java/com/jrjr/inbest/oauth/handler/OAuth2AuthenticationSuccessHandler.java
+++ spring/src/main/java/com/jrjr/inbest/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,7 +1,5 @@
 package com.jrjr.inbest.oauth.handler;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Set;
 
 import org.springframework.security.core.Authentication;
@@ -9,13 +7,11 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jrjr.inbest.global.util.CookieUtil;
 import com.jrjr.inbest.jwt.dto.AccessTokenDto;
 import com.jrjr.inbest.jwt.service.JwtProvider;
 import com.jrjr.inbest.login.constant.Role;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -27,30 +23,29 @@ import lombok.extern.slf4j.Slf4j;
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final JwtProvider jwtProvider;
-	private final ObjectMapper objectMapper;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) throws IOException, ServletException {
+		Authentication authentication) {
 		log.info("OAuth2AuthenticationSuccessHandler 실행 - OAuth2 인증 성공");
+
+		log.info("authentication.getName(): {}", authentication.getName());
 		Set<String> roleSet = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 		Role role = Role.ROLE_USER;
 		if (roleSet.contains(Role.ROLE_ADMIN.toString())) {
 			role = Role.ROLE_ADMIN;
 		}
-		log.info("authentication.getName(): {}", authentication.getName());
 		log.info("role: {}", role);
+
 		AccessTokenDto accessTokenDto = jwtProvider.generateAccessToken(authentication.getName(), role);
 		log.info("accessTokenDto: {}", accessTokenDto);
 
 		String refreshToken = jwtProvider.generateRefreshToken(authentication.getName());
 		log.info("refreshToken: {}", refreshToken);
 
-		CookieUtil.createCookie(response, "refreshToken", refreshToken);
+		response.setHeader("grantType", accessTokenDto.getGrantType());
+		response.setHeader("accessToken", accessTokenDto.getAccessToken());
 
-		response.setContentType("application/json;charset=UTF-8");
-		PrintWriter writer = response.getWriter();
-		writer.println(objectMapper.writeValueAsString(accessTokenDto));
-		writer.flush();
+		CookieUtil.createCookie(response, "refreshToken", refreshToken);
 	}
 }
