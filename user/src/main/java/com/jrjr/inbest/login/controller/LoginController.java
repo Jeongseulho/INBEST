9de9d629b5cff3142df4a4090a677080fe39d8c5
Hc-5514 <tbd05050@naver.com>
2023-09-08 18:16:54 +0900

    refact : LoginController 수정 (RequestMapping 변경)
    "/login" -> "/api/login"
    
    20230908 이호찬

--- user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
+++ user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/login")
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
