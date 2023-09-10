0a331bcd1fce87c2690e2e27388a16d8d72899a1
Hc-5514 <tbd05050@naver.com>
2023-09-10 15:07:28 +0900

    refact : LoginServiceImpl 예외 수정
    IllegalArgumentException 세분화
    -> AuthenticationFailedException (회원 정보 없음, 비밀번호 불일치, 탈퇴한 회원)
    -> DuplicateException (이미 로그인 중인 계정)
    
    20230910 이호찬

--- user/src/main/java/com/jrjr/inbest/login/service/LoginServiceImpl.java
+++ user/src/main/java/com/jrjr/inbest/login/service/LoginServiceImpl.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.jrjr.inbest.jwt.repository.RefreshTokenRepository;
 import com.jrjr.inbest.login.dto.LoginDto;
 import com.jrjr.inbest.login.entity.Login;
+import com.jrjr.inbest.login.exception.AuthenticationFailedException;
+import com.jrjr.inbest.login.exception.DuplicateException;
 import com.jrjr.inbest.login.repository.LoginRepository;
 import com.jrjr.inbest.user.entity.User;
 import com.jrjr.inbest.user.repository.UserRepository;
@@ -32,22 +34,22 @@ public class LoginServiceImpl implements LoginService {
 		Optional<Login> loginEntity = loginRepository.findByEmail(inputLoginDto.getEmail());
 		Optional<User> userEntity = userRepository.findByEmail(inputLoginDto.getEmail());
 		if (loginEntity.isEmpty() || userEntity.isEmpty()) {
-			throw new IllegalArgumentException("일치하는 회원 정보가 없다는 에러 띄우기"); // 401
+			throw new AuthenticationFailedException("회원 정보 없음");
 		}
 
 		// 탈퇴 확인
 		if (userEntity.get().getDeletedDate() != null) {
-			throw new IllegalArgumentException("일치하는 회원 정보가 없다는 에러 띄우기"); // 401
+			throw new AuthenticationFailedException("탈퇴한 회원");
 		}
 
 		// 비밀번호 일치 확인
 		if (!passwordEncoder.matches(inputLoginDto.getPassword(), loginEntity.get().getPassword())) {
-			throw new IllegalArgumentException("일치하는 회원 정보가 없다는 에러 띄우기"); // 401
+			throw new AuthenticationFailedException("비밀번호 불일치");
 		}
 
 		// 중복 로그인 방지
 		if (refreshTokenRepository.existsById(inputLoginDto.getEmail())) {
-			throw new IllegalArgumentException("이미 로그인 중이라는 에러 띄우기"); //
+			throw new DuplicateException("이미 로그인 중인 계정");
 		}
 
 		return LoginDto.builder()
@@ -62,12 +64,12 @@ public class LoginServiceImpl implements LoginService {
 
 		Optional<Login> loginEntity = loginRepository.findByEmail(inputLoginDto.getEmail());
 		if (loginEntity.isEmpty()) {
-			throw new IllegalArgumentException("일치하는 회원 정보가 없다는 에러 띄우기"); // 401
+			throw new AuthenticationFailedException("회원 정보 없음");
 		}
 
 		// 비밀번호 일치 확인
 		if (!passwordEncoder.matches(inputLoginDto.getPassword(), loginEntity.get().getPassword())) {
-			throw new IllegalArgumentException("일치하는 회원 정보가 없다는 에러 띄우기"); // 401
+			throw new AuthenticationFailedException("비밀번호 불일치");
 		}
 
 		// redis 에서 refreshToken 삭제
