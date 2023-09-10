b870fdff154fcb69efff0fd9bd433e1508067ca5
Hc-5514 <tbd05050@naver.com>
2023-09-10 17:57:37 +0900

    feat : PasswordEncoderConfig 추가
    SecurityConfig 에서 PasswordEncoder 분리 (순환 참조 해결)
    
    20230910 이호찬

--- user/src/main/java/com/jrjr/inbest/config/SecurityConfig.java
+++ user/src/main/java/com/jrjr/inbest/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
@@ -73,10 +71,5 @@ public class SecurityConfig {
 		);
 
 		return http.build();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 }
