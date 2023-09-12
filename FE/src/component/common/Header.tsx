221ef711c9990f324e60a1e6943ab9b898373cda
Jonryul <kjr5876@gmail.com>
2023-09-12 16:14:47 +0900

    feat : 프로필 메뉴바 추가
    20230912 jongRyul

--- FE/src/component/common/Header.tsx
+++ FE/src/component/common/Header.tsx
@@ -1,5 +1,14 @@
 import { Link } from "react-router-dom";
 import temp_logo from "../../asset/image/temp_logo.png";
+import defaultImg from "../../asset/image/default_image.png";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { FiLogOut } from "react-icons/fi";
+import { AiFillProfile } from "react-icons/ai";
+import { BsPencilSquare } from "react-icons/bs";
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Header = () => {
   return (
@@ -16,7 +25,71 @@ const Header = () => {
       <Link to="/login" className=" text-center me-56 text-xl">
         로그인
       </Link>
-      <div className=" hover:cursor-pointer">프로필 사진</div>
+      {/* <div className=" hover:cursor-pointer">프로필 사진</div> */}
+      <Menu as="div" className="relative inline-block text-left me-10">
+        <div>
+          <Menu.Button className="flex justify-center items-center">
+            <img src={defaultImg} className="h-12 w-12 rounded-full" />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className=" z-20 absolute -right-1  mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      " px-4 py-2  flex items-center"
+                    )}
+                  >
+                    <AiFillProfile />
+                    <span className="ms-3">마이페이지</span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      " px-4 py-2 flex items-center"
+                    )}
+                  >
+                    <BsPencilSquare />
+                    <span className="ms-3">회원정보 수정</span>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                      "px-4 py-2 flex items-center text-red-500"
+                    )}
+                  >
+                    <FiLogOut />
+                    <span className="ms-3">로그아웃</span>
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </header>
   );
 };
