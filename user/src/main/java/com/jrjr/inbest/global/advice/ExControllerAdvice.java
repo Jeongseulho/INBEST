b66e85ca5c5a3cb62d7175715175e208ba7542f2
Hc-5514 <tbd05050@naver.com>
2023-09-12 13:16:03 +0900

    chore : AuthenticationFailedException 패키지 이동
    AuthenticationFailedException 패키지 이동
    패키지 이동에 따른 ExControllerAdvice, LoginServiceImpl import 변경
    
    20230912 이호찬

--- user/src/main/java/com/jrjr/inbest/global/advice/ExControllerAdvice.java
+++ user/src/main/java/com/jrjr/inbest/global/advice/ExControllerAdvice.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.jrjr.inbest.email.exception.InvalidVerificationCodeException;
 import com.jrjr.inbest.global.dto.ErrorResult;
+import com.jrjr.inbest.global.exception.AuthenticationFailedException;
 import com.jrjr.inbest.global.exception.DuplicateException;
 import com.jrjr.inbest.global.exception.NotFoundException;
-import com.jrjr.inbest.login.exception.AuthenticationFailedException;
 
 import lombok.extern.slf4j.Slf4j;
 
