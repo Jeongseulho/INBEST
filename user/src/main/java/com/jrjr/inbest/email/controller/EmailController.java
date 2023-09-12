5904fb36a7d4a8057b6345daf5600817cf4e60bc
Hc-5514 <tbd05050@naver.com>
2023-09-12 16:15:28 +0900

    refact : EmailController, LoginController, UserController (mapping url 수정)
    "/api" prefix 제거
    
    20230912 이호찬

--- user/src/main/java/com/jrjr/inbest/email/controller/EmailController.java
+++ user/src/main/java/com/jrjr/inbest/email/controller/EmailController.java
@@ -16,7 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/email")
+@RequestMapping("/email")
 @RequiredArgsConstructor
 @Slf4j
 public class EmailController {
--- user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
+++ user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/login")
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
