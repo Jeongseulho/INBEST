6cc7d45ca2f446e7051e14148a4f7fdbf1990ccb
Hc-5514 <tbd05050@naver.com>
2023-09-13 17:55:50 +0900

    fix : UserController 수정
    @RequestBody -> @ModelAttribute 변경
    
    20230913 이호찬

--- user/src/main/java/com/jrjr/inbest/user/controller/UserController.java
+++ user/src/main/java/com/jrjr/inbest/user/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -113,7 +114,7 @@ public class UserController {
 	@PostMapping("{seq}")
 	ResponseEntity<Map<String, Object>> updateProfile(@PathVariable(value = "seq") Long seq,
 		@RequestParam(value = "file", required = false) MultipartFile file,
-		@RequestBody UserDto userDto,
+		@ModelAttribute UserDto userDto,
 		HttpServletRequest request) throws IOException {
 		log.info("UserController - updateProfile 실행: {}", seq);
 		Map<String, Object> resultMap = new HashMap<>();
