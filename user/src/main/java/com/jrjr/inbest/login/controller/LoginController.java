2d948e5c02d49a54c9fda812efe8f18b9d237f65
Hc-5514 <tbd05050@naver.com>
2023-09-14 17:09:51 +0900

    feat : LoginController (로그인 성공 시 반환값 수정, Swagger 적용)
    토큰 정보, 유저 pk 값, 프로필 이미지 경로, 권한, 로그인 경로 반환
    
    20230914 이호찬

--- user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
+++ user/src/main/java/com/jrjr/inbest/login/controller/LoginController.java
@@ -15,7 +15,12 @@ import com.jrjr.inbest.jwt.dto.AccessTokenDto;
 import com.jrjr.inbest.jwt.service.JwtProvider;
 import com.jrjr.inbest.login.dto.LoginDto;
 import com.jrjr.inbest.login.service.LoginService;
+import com.jrjr.inbest.user.dto.UserDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,11 +29,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/login")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "로그인/로그아웃", description = "로그인/로그아웃 API")
 public class LoginController {
 
 	private final LoginService loginService;
 	private final JwtProvider jwtProvider;
 
+	@Operation(summary = "로그인", description = "필수 값: email, password")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "grantType, accessToken, seq, profileImgSearchName, role, provider 반환"),
+		@ApiResponse(responseCode = "401", description = "INVALID_USER (회원 정보 없음, 탈퇴한 회원, 비밀번호 불일치)"),
+		@ApiResponse(responseCode = "409", description = "이미 로그인 중인 계정")
+	})
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, Object>> login(@RequestBody LoginDto inputLoginDto,
 		HttpServletResponse response) {
@@ -36,22 +48,30 @@ public class LoginController {
 		Map<String, Object> resultMap = new HashMap<>();
 
 		// 인증 후 권한 확인
-		LoginDto outputLoginDto = loginService.login(inputLoginDto);
+		UserDto userDto = loginService.login(inputLoginDto);
 
 		// refreshToken 생성 후 cookie 저장
-		CookieUtil.createCookie(response, "refreshToken", jwtProvider.generateRefreshToken(outputLoginDto.getEmail()));
+		CookieUtil.createCookie(response, "refreshToken", jwtProvider.generateRefreshToken(userDto.getEmail()));
 
 		// accessToken 생성 후 반환
 		AccessTokenDto accessTokenDto
-			= jwtProvider.generateAccessToken(outputLoginDto.getEmail(), outputLoginDto.getRole());
+			= jwtProvider.generateAccessToken(userDto.getEmail(), userDto.getRole());
 
 		resultMap.put("success", true);
 		resultMap.put("grantType", accessTokenDto.getGrantType());
 		resultMap.put("accessToken", accessTokenDto.getAccessToken());
-		resultMap.put("email", inputLoginDto.getEmail());
+		resultMap.put("seq", userDto.getSeq());
+		resultMap.put("profileImgSearchName", userDto.getProfileImgSearchName());
+		resultMap.put("role", userDto.getRole());
+		resultMap.put("provider", userDto.getProvider());
 		return new ResponseEntity<>(resultMap, HttpStatus.OK);
 	}
 
+	@Operation(summary = "로그아웃", description = "필수 값: email, password")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "성공"),
+		@ApiResponse(responseCode = "401", description = "INVALID_USER (회원 정보 없음, 비밀번호 불일치)"),
+	})
 	@PostMapping("/logout")
 	public ResponseEntity<Map<String, Object>> logout(@RequestBody LoginDto inputLoginDto,
 		HttpServletResponse response) {
