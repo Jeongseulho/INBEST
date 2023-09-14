f3bcd9d163334a38f5682bdff40a002c9f98862b
Hc-5514 <tbd05050@naver.com>
2023-09-14 17:10:41 +0900

    docs : LoginDto (Swagger 적용)
    Dto 설명 추가
    
    20230914 이호찬

--- user/src/main/java/com/jrjr/inbest/login/dto/LoginDto.java
+++ user/src/main/java/com/jrjr/inbest/login/dto/LoginDto.java
@@ -2,6 +2,8 @@ package com.jrjr.inbest.login.dto;
 
 import com.jrjr.inbest.login.constant.Role;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,11 +16,17 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Schema(description = "로그인 정보")
 public class LoginDto {
 
+	@NotBlank
+	@Schema(description = "이메일", type = "String", example = "tbd05050@naver.com")
 	private String email;
 
+	@NotBlank
+	@Schema(description = "비밀번호", type = "String", example = "abc1234")
 	private String password;
 
+	@Schema(hidden = true)
 	private Role role;
 }
