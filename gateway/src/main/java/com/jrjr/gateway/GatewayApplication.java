0746880fad8d09c92f9e9d475f27997030821919
SuHyun Kim <khnemu@naver.com>
2023-09-18 13:09:42 +0900

    feat : apllication.yml을 config에서 불러오는 것으로 수정
    20230911 KimSuHyun

--- gateway/src/main/java/com/jrjr/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jrjr/gateway/GatewayApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
