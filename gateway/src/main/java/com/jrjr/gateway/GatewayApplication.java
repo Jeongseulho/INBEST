120b7524ad1b860b5d2c0163589c57028e192761
Hc-5514 <tbd05050@naver.com>
2023-09-18 12:50:08 +0900

    style : GatewayApplication (coding convention 적용)
    naver coding convention 적용
    
    20230918 hoChan

--- gateway/src/main/java/com/jrjr/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jrjr/gateway/GatewayApplication.java
@@ -2,13 +2,12 @@ package com.jrjr.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class GatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 
 }
