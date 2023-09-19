2d8a46c3ae9850e8bfedcaf92c885bbbd8d621d2
Hc-5514 <tbd05050@naver.com>
2023-09-20 02:22:45 +0900

    feat : SecurityResponse 추가 (Security Server 로부터 받을 응답)
    Security Server 로부터 받을 응답 (success, message)
    
    20230920 hoChan

---
+++ gateway/src/main/java/com/jrjr/gateway/dto/SecurityResponse.java
@@ -0,0 +1,13 @@
+package com.jrjr.gateway.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SecurityResponse {
+
+	private String success;
+
+	private String message;
+}
