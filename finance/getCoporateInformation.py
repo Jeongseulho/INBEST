67383449448dcc19a3c6de68198a98bffc7bf9e6
장진욱 <>
2023-09-19 15:32:39 +0900

    etc : model.py 수정 및 기업 data 생성 기능 추가 20230919 jinWook

--- finance/getCoporateInformation.py
+++ finance/getCoporateInformation.py
@@ -2,6 +2,12 @@ import requests
 import zipfile
 from io import BytesIO
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "finance.settings")
+
+import django
+django.setup()
+
 # Opendart API 엔드포인트 URL
 url = "https://opendart.fss.or.kr/api/corpCode.xml"
 
@@ -11,7 +17,6 @@ api_key = "ac30ceeeb8ac94447e5e065876a5783d79e5b3ec"
 params = {
     "crtfc_key": api_key,
 }
-
 
 # API 호출
 response = requests.get(url, params=params)
@@ -32,6 +37,9 @@ if response.status_code == 200:
     root = ET.fromstring(xml_data)
     corp_codes = root.findall(".//list")
 
+    # 저장해야하는 모델 위치
+    from financialData.models import Company
+
     # 상장 기업 정보를 저장할 리스트
     listed_companies = []
 
@@ -42,16 +50,12 @@ if response.status_code == 200:
 
         # 상장된 기업만 파싱
         if len(stock_code) != 1:
-                listed_companies.append({
-                    "corp_name": corp_name,
-                    "corp_code": corp_code.find('corp_code').text,
-                    "stock_code": stock_code
-                })
-    for company in listed_companies:
-         print(f"회사명 : {company['corp_name']}, 고유번호 : {company['corp_code']}, 주식코드 : {company['stock_code']} ")
-    
-    print(len(listed_companies))
-
+            company = Company(
+                company_name=corp_name,
+                company_code=corp_code.find('corp_code').text,
+                company_stock_code=stock_code
+            )
+            company.save()
 
 else:
     print("API 호출 실패:", response.status_code)
