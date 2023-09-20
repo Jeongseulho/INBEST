6d74e87c09b2b6241090c1f9065002a2739e81e2
Jeong seulho <wjdtmfgh@gmail.com>
2023-09-20 13:12:04 +0900

    feat : info 탭 UI
    20230920 seulHo

--- FE/src/component/common/DecreaseIcon.tsx
+++ FE/src/component/common/DecreaseIcon.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const DecreaseIcon = ({ number }: Props) => {
   return (
-    <div className=" flex items-center rounded-full bg-myRed bg-opacity-10 py-1 px-3 mx-3">
+    <div className="h-6 flex items-center rounded-full bg-red-300 py-1 px-3 mx-3">
       <div id="decrease-triangle" className=" me-2"></div>
       <p className=" text-myRed font-extraBold">{number}%</p>
     </div>
--- FE/src/component/investing-info/organisms/Ranking.tsx
+++ FE/src/component/investing-info/organisms/Ranking.tsx
@@ -1,4 +1,88 @@
+import Ranker from "../molecules/Ranker";
+import NormalRanking from "../molecules/NormalRanking";
+
 const Ranking = () => {
-  return <div className=" shadow-component col-span-3 row-span-full">Ranking</div>;
+  const rankers = [
+    {
+      ranking: 1,
+      profileImg: "",
+      nickname: "이름1",
+      money: 1000000,
+      percentage: 20,
+      companyIcon: ["", "", ""],
+    },
+    {
+      ranking: 2,
+      profileImg: "",
+      nickname: "이름2",
+      money: 119357237,
+      percentage: 100,
+      companyIcon: ["", ""],
+    },
+    {
+      ranking: 3,
+      profileImg: "",
+      nickname: "이름3",
+      money: 4843434,
+      percentage: -10,
+      companyIcon: [],
+    },
+  ];
+
+  const normalRankers = [
+    {
+      ranking: 4,
+      profileImg: "",
+      nickname: "이름4",
+      money: 125789557,
+      percentage: 100,
+    },
+    {
+      ranking: 5,
+      profileImg: "",
+      nickname: "이름5",
+      money: 119357237,
+      percentage: -10,
+    },
+    {
+      ranking: 6,
+      profileImg: "",
+      nickname: "이름6",
+      money: 4843434,
+      percentage: -10,
+    },
+    {
+      ranking: 7,
+      profileImg: "",
+      nickname: "이름7",
+      money: 125789557,
+      percentage: 40,
+    },
+  ];
+  return (
+    <div className=" shadow-component col-span-3 row-span-full flex flex-col gap-4 p-4">
+      {rankers.map((ranker) => (
+        <Ranker
+          key={ranker.ranking}
+          ranking={ranker.ranking}
+          profileImg={ranker.profileImg}
+          nickname={ranker.nickname}
+          money={ranker.money}
+          percentage={ranker.percentage}
+          companyIcon={ranker.companyIcon}
+        />
+      ))}
+      {normalRankers.map((ranker) => (
+        <NormalRanking
+          key={ranker.ranking}
+          ranking={ranker.ranking}
+          profileImg={ranker.profileImg}
+          nickname={ranker.nickname}
+          money={ranker.money}
+          percentage={ranker.percentage}
+        />
+      ))}
+    </div>
+  );
 };
 export default Ranking;
