9e04dabc38c033426a3f6de12d5560854d63a7f5
장진욱 <>
2023-09-20 15:59:40 +0900

    feat : 재무제표 Table 생성 스크립트 작성 20230920 jinWook

--- finance/financialData/models.py
+++ finance/financialData/models.py
@@ -24,3 +24,47 @@ class Company(models.Model):
 
     def __str__(self):
         return self.company_name
+
+class FinancialStatement(models.Model):
+    seq = models.AutoField(primary_key=True)
+    company_seq = models.ForeignKey(
+        Company, 
+        on_delete=models.CASCADE, 
+        related_name='financial_statements'
+    )
+    current_assets = models.BigIntegerField()  # 유동자산
+    non_current_assets = models.BigIntegerField()  # 비유동자산
+    total_assets_first = models.BigIntegerField()  # 자산총계 1분기
+    total_assets_second = models.BigIntegerField()  # 자산총계 2분기
+    total_assets_third = models.BigIntegerField()  # 자산총계 3분기
+    current_liabilities = models.BigIntegerField()  # 유동부채
+    non_current_liabilities = models.BigIntegerField()  # 비유동부채
+    total_liabilities_first = models.BigIntegerField()  # 부채총계 1분기
+    total_liabilities_second = models.BigIntegerField()  # 부채총계 2분기
+    total_liabilities_third = models.BigIntegerField()  # 부채총계 3분기
+    capital = models.BigIntegerField()  # 자본금
+    total_equity = models.BigIntegerField()  # 자본총계
+    revenue_first = models.BigIntegerField()  # 매출액 1분기
+    revenue_second = models.BigIntegerField()  # 매출액 2분기
+    revenue_third = models.BigIntegerField()  # 매출액 3분기
+    gross_profit = models.BigIntegerField()  # 매출총이익
+    operating_profit_first = models.BigIntegerField()  # 영업이익 1분기
+    operating_profit_second = models.BigIntegerField()  # 영업이익 2분기
+    operating_profit_third = models.BigIntegerField()  # 영업이익 3분기
+    non_operating_income = models.BigIntegerField()  # 영업외수익
+    non_operating_expenses = models.BigIntegerField()  # 영업외비용
+    income_before_tax = models.BigIntegerField()  # 법인세차감전이익
+    income_tax_expense = models.BigIntegerField()  # 법인세비용
+    net_income = models.BigIntegerField()  # 총당기순이익
+    total_asset_growth_rate = models.FloatField()  # 총자산증가율
+    revenue_asset_growth_rate = models.FloatField()  # 매출액자산증가율
+    net_income_growth_rate = models.FloatField()  # 순이익증가율
+    operating_profit_margin = models.FloatField()  # 영업이익률
+    roe = models.FloatField()  # ROE
+    roic = models.FloatField()  # ROIC
+    debt_to_equity_ratio = models.FloatField()  # 부채비율
+    interest_coverage_ratio = models.FloatField()  # 이자보상배수
+    debt_dependency = models.FloatField()  # 차입금의존도
+    accounts_receivable_turnover = models.FloatField()  # 매출채권회전율
+    inventory_turnover = models.FloatField()  # 재고자산회전율
+    total_asset_turnover = models.FloatField()  # 총자본회전율
