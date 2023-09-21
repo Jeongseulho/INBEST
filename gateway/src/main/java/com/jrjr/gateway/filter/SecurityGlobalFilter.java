8ad34a9170047fbcd635604a193d88baf061d466
Hc-5514 <tbd05050@naver.com>
2023-09-21 10:59:18 +0900

    test : SecurityGlobalFilter 미적용
    SecurityGlobalFilter 미적용
    
    20230921 hoChan

--- gateway/src/main/java/com/jrjr/gateway/filter/SecurityGlobalFilter.java
+++ gateway/src/main/java/com/jrjr/gateway/filter/SecurityGlobalFilter.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
 
@@ -20,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Component
+// @Component
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityGlobalFilter implements GlobalFilter {
