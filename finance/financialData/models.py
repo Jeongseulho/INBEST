330a7d172fa0b055a6adec2a73f431115b73fcb0
장진욱 <>
2023-09-21 12:16:26 +0900

    etc : 재무제표 DB 작성중 임시 커밋 20230921 jinWook

--- finance/financialData/models.py
+++ finance/financialData/models.py
@@ -27,44 +27,43 @@ class Company(models.Model):
 
 class FinancialStatement(models.Model):
     seq = models.AutoField(primary_key=True)
-    company_seq = models.ForeignKey(
-        Company, 
-        on_delete=models.CASCADE, 
-        related_name='financial_statements'
-    )
-    current_assets = models.BigIntegerField()  # 유동자산
-    non_current_assets = models.BigIntegerField()  # 비유동자산
-    total_assets_first = models.BigIntegerField()  # 자산총계 1분기
-    total_assets_second = models.BigIntegerField()  # 자산총계 2분기
-    total_assets_third = models.BigIntegerField()  # 자산총계 3분기
-    current_liabilities = models.BigIntegerField()  # 유동부채
-    non_current_liabilities = models.BigIntegerField()  # 비유동부채
-    total_liabilities_first = models.BigIntegerField()  # 부채총계 1분기
-    total_liabilities_second = models.BigIntegerField()  # 부채총계 2분기
-    total_liabilities_third = models.BigIntegerField()  # 부채총계 3분기
-    capital = models.BigIntegerField()  # 자본금
-    total_equity = models.BigIntegerField()  # 자본총계
-    revenue_first = models.BigIntegerField()  # 매출액 1분기
-    revenue_second = models.BigIntegerField()  # 매출액 2분기
-    revenue_third = models.BigIntegerField()  # 매출액 3분기
-    gross_profit = models.BigIntegerField()  # 매출총이익
-    operating_profit_first = models.BigIntegerField()  # 영업이익 1분기
-    operating_profit_second = models.BigIntegerField()  # 영업이익 2분기
-    operating_profit_third = models.BigIntegerField()  # 영업이익 3분기
-    non_operating_income = models.BigIntegerField()  # 영업외수익
-    non_operating_expenses = models.BigIntegerField()  # 영업외비용
-    income_before_tax = models.BigIntegerField()  # 법인세차감전이익
-    income_tax_expense = models.BigIntegerField()  # 법인세비용
-    net_income = models.BigIntegerField()  # 총당기순이익
-    total_asset_growth_rate = models.FloatField()  # 총자산증가율
-    revenue_asset_growth_rate = models.FloatField()  # 매출액자산증가율
-    net_income_growth_rate = models.FloatField()  # 순이익증가율
-    operating_profit_margin = models.FloatField()  # 영업이익률
-    roe = models.FloatField()  # ROE
-    roic = models.FloatField()  # ROIC
-    debt_to_equity_ratio = models.FloatField()  # 부채비율
-    interest_coverage_ratio = models.FloatField()  # 이자보상배수
-    debt_dependency = models.FloatField()  # 차입금의존도
-    accounts_receivable_turnover = models.FloatField()  # 매출채권회전율
-    inventory_turnover = models.FloatField()  # 재고자산회전율
-    total_asset_turnover = models.FloatField()  # 총자본회전율
+    company_seq = models.ForeignKey('Company', on_delete=models.CASCADE, related_name='financial_statements')
+    current_assets = models.BigIntegerField(default=0)  # 유동자산
+    non_current_assets = models.BigIntegerField(default=0)  # 비유동자산
+    total_assets_first = models.BigIntegerField(default=0)  # 자산총계 2분기
+    total_assets_second = models.BigIntegerField(default=0)  # 자산총계 4분기
+    current_liabilities = models.BigIntegerField(default=0)  # 유동부채
+    non_current_liabilities = models.BigIntegerField(default=0)  # 비유동부채
+    total_liabilities_first = models.BigIntegerField(default=0)  # 부채총계 2분기
+    total_liabilities_second = models.BigIntegerField(default=0)  # 부채총계 4분기
+    capital = models.BigIntegerField(default=0)  # 자본금
+    total_equity = models.BigIntegerField(default=0)  # 자본총계
+
+    revenue_first = models.BigIntegerField(default=0)  # 매출액 2분기
+    revenue_second = models.BigIntegerField(default=0)  # 매출액 4분기
+    revenue_2020 = models.BigIntegerField(default=0)  # 2020년 매출액
+    revenue_2021 = models.BigIntegerField(default=0)  # 2021년 매출액
+    gross_profit = models.BigIntegerField(default=0)  # 매출총이익
+    operating_profit_first = models.BigIntegerField(default=0)  # 영업이익 2분기
+    operating_profit_second = models.BigIntegerField(default=0)  # 영업이익 4분기
+    non_operating_income = models.BigIntegerField(default=0)  # 영업외수익
+    non_operating_expenses = models.BigIntegerField(default=0)  # 영업외비용
+    income_before_tax = models.BigIntegerField(default=0)  # 법인세차감전이익
+    income_tax_expense = models.BigIntegerField(default=0)  # 법인세비용
+    net_income = models.BigIntegerField(default=0)  # 총당기순이익
+    net_income_2020 = models.BigIntegerField(default=0)  # 2020년 총단기순이익
+    net_income_2021 = models.BigIntegerField(default=0)  # 2021년 총단기순이익
+
+    total_asset_growth_rate = models.FloatField(default=0.0)  # 총자산증가율
+    revenue_asset_growth_rate = models.FloatField(default=0.0)  # 매출액자산증가율
+    net_income_growth_rate = models.FloatField(default=0.0)  # 순이익증가율
+    operating_profit_margin = models.FloatField(default=0.0)  # 영업이익률
+    roe = models.FloatField(default=0.0)  # ROE
+    roic = models.FloatField(default=0.0)  # ROIC
+    debt_to_equity_ratio = models.FloatField(default=0.0)  # 부채비율
+    interest_coverage_ratio = models.FloatField(default=0.0)  # 이자보상배수
+    debt_dependency = models.FloatField(default=0.0)  # 차입금의존도
+    accounts_receivable_turnover = models.FloatField(default=0.0)  # 매출채권회전율
+    inventory_turnover = models.FloatField(default=0.0)  # 재고자산회전율
+    total_asset_turnover = models.FloatField(default=0.0)  # 총자본회전율
+    
--- finance/getfinancialStatement.py
+++ finance/getfinancialStatement.py
@@ -8,25 +8,29 @@ django.setup()
 from financialData.models import Company, FinancialStatement
 
 url = "https://opendart.fss.or.kr/api/fnlttSinglAcntAll.json"  
-api_key = "36509c5feed29d3468703dc40d3a64536d7cf944"
+api_key = "ac30ceeeb8ac94447e5e065876a5783d79e5b3ec"
 
 # 모든 Company 모델의 company_code를 가져옴
-company_codes = Company.objects.values_list('company_code', flat=True)[:1]
+company_codes = Company.objects.values_list('company_code', flat=True)[500:800]
 
 for company_code in company_codes:
     # 요청 파라미터 설정
     params = {
         "crtfc_key": api_key,
         "corp_code": company_code,
-        "bsns_year": "2023",
-        "reprt_code" : "11012",
+        "bsns_year": "2022",
+        "reprt_code" : "11013",
         "fs_div" : "OFS"
-    }   
+    }
+
+    print(company_code)
 
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         statement = response.json()
+
+        print(statement)
 
         for item in statement.get('list', []):
             account_name = item.get('account_nm')
@@ -34,6 +38,7 @@ for company_code in company_codes:
 
             if account_name == "유동자산":
                 # 해당 계정명이 "유동자산"인 경우 current_assets 필드에 저장
+
                 try:
                     company = Company.objects.get(company_code=company_code)
                     financial_statement = FinancialStatement(
