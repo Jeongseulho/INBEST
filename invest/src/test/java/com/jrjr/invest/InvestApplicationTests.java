a667f8073daaef8d310adee5a31caf6a8c28a079
SuHyun Kim <khnemu@naver.com>
2023-09-21 14:04:40 +0900

    feat : spring 테스트 코드 주석
    20230921 suHyun

--- invest/src/test/java/com/jrjr/invest/InvestApplicationTests.java
+++ invest/src/test/java/com/jrjr/invest/InvestApplicationTests.java
@@ -1,13 +1,12 @@
-package com.jrjr.invest;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InvestApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+// package com.jrjr.invest;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class InvestApplicationTests {
+//
+// 	@Test
+// 	void contextLoads() {
+// 	}
+// }
