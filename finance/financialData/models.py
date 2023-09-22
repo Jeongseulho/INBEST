9f5f565ca75aafa9011b040bb97a10ddb52c35ad
장진욱 <>
2023-09-22 09:37:02 +0900

    fix : 산업코드 table  삭제 및 병합 20230922 jinWook

--- finance/financialData/models.py
+++ finance/financialData/models.py
@@ -1,28 +1,11 @@
 from django.db import models
-
-class CompanyIndustry(models.Model):
-    company_industry_code = models.IntegerField(primary_key=True)
-    industry_name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.industry_name
-
 class Company(models.Model):
     seq = models.AutoField(primary_key=True)
     company_code = models.CharField(max_length=10)
     company_name = models.CharField(max_length=255)
-    # company_industry_code = models.IntegerField(default=None, null=True)
     company_stock_code = models.CharField(max_length=10)
     company_real_industry_code = models.CharField(max_length=10, null=True)
-
-    company_industry_code = models.ManyToManyField(
-        CompanyIndustry,
-        null=True,
-        related_name='company',
-    )
-
-    def __str__(self):
-        return self.company_name
+    company_industry = models.CharField(max_length=50)
 
 class FinancialStatement(models.Model):
     seq = models.AutoField(primary_key=True)
--- finance/getCompanyIndustrycodeByCompanyRealIndustrycode.py
+++ finance/getCompanyIndustrycodeByCompanyRealIndustrycode.py
@@ -11,9 +11,10 @@ print('django setup 성공')
 apps.populate(apps.get_app_configs())
 print('초기화 성공')
 
-from financialData.models import Company, CompanyIndustry
+from financialData.models import Company
 companies = Company.objects.all()
 
+Working = 0
 for company in companies:
     company_code = company.company_real_industry_code
 
@@ -22,42 +23,45 @@ for company in companies:
         
         industry_code = int(first_two_digits)  # 정수로 변환
 
+        print(f'Working : {Working}' )
+        Working += 1
+
         if 1 <= industry_code <= 3:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=1)
+            company.company_industry = "agriculture_forestry_fishing"
         elif 5 <= industry_code <= 8:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=2)
+            company.company_industry = "mining"
         elif 10 <= industry_code <= 34:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=3)
+            company.company_industry = "manufacturing"
         elif industry_code == 35:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=4)
+            company.company_industry = "electricity_gas_supply"
         elif 36 <= industry_code <= 39:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=5)
+            company.company_industry = "water_waste_recycling"
         elif 41 <= industry_code <= 42:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=6)
+            company.company_industry = "construction"
         elif 45 <= industry_code <= 47:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=7)
+            company.company_industry = "wholesale_retail_trade"
         elif 49 <= industry_code <= 52:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=8)
+            company.company_industry = "transportation_warehousing"
         elif 55 <= industry_code <= 56:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=9)
+            company.company_industry = "accommodation_food_services"
         elif 58 <= industry_code <= 63:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=10)
+            company.company_industry = "information_communication"
         elif 64 <= industry_code <= 66:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=11)
+            company.company_industry = "financial_insurance"
         elif industry_code == 68:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=12)
+            company.company_industry = "real_estate"
         elif 70 <= industry_code <= 73:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=13)
+            company.company_industry = "professional_scientific_technical_services"
         elif 74 <= industry_code <= 76:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=14)
+            company.company_industry = "business_support_rental_services"
         elif industry_code == 84:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=15)
+            company.company_industry = "public_administration"
         elif industry_code == 85:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=16)
+            company.company_industry = "education"
         elif 86 <= industry_code <= 87:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=17)
+            company.company_industry = "health_social_services"
         elif 90 <= industry_code <= 91:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=18)
+            company.company_industry = "arts_sports_recreation"
         else:
-            company.company_industry_code = CompanyIndustry.objects.get(company_industry_code=19)
+            company.company_industry = "other_industry"
         company.save() 
