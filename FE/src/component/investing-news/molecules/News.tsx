1c2ae6d34fb30a79f391acf8a3b31adf526a4acc
Jeong seulho <wjdtmfgh@gmail.com>
2023-09-22 10:45:34 +0900

    feat : main news 구현
    20230922 seulHo

--- FE/src/component/investing-news/molecules/News.tsx
+++ FE/src/component/investing-news/molecules/News.tsx
@@ -7,11 +7,15 @@ interface Props {
 
 const News = ({ title, content, imgUrl, linkUrl }: Props) => {
   return (
-    <a className=" flex items-center" href={linkUrl} target="_black">
-      <img src={imgUrl} />
+    <a
+      className=" flex items-center gap-3 hover:bg-gray-200 hover:bg-opacity-70 rounded-md transition-colors duration-300"
+      href={linkUrl}
+      target="_black"
+    >
+      <img src={imgUrl} width={100} />
       <div className=" flex flex-col">
         <p>{title}</p>
-        <p>{content}</p>
+        <p className=" text-myGray font-light text-sm">{content}</p>
       </div>
     </a>
   );
--- FE/src/component/investing-news/organisms/MainNews.tsx
+++ FE/src/component/investing-news/organisms/MainNews.tsx
@@ -1,11 +1,22 @@
 import News from "../../investing-news/molecules/News";
+import { useMainNews } from "./useMainNews";
 
 const MainNews = () => {
-  // const { data, isLoading } = useMainNews();
+  const { data, isLoading, isError } = useMainNews();
   return (
     <div className=" shadow-component col-span-1 row-span-2 p-4">
-      <h3>주요 뉴스</h3>
-      <News title="제목" content="내용......" imgUrl="" linkUrl="" />
+      <h3 className=" mb-6">주요 뉴스</h3>
+      {isLoading ? (
+        <p>로딩중...</p>
+      ) : isError ? (
+        <p>에러가 발생했습니다.</p>
+      ) : (
+        <div className=" flex flex-col gap-4 ">
+          {data?.map((news, idx) => (
+            <News key={idx} title={news.title} content={news.content} imgUrl={news.image_url} linkUrl={news.link_url} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
