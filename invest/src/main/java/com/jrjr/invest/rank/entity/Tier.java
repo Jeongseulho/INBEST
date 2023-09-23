59de9c505896e221d06314867bd5301dd566545f
Hc-5514 <tbd05050@naver.com>
2023-09-23 16:16:17 +0900

    feat : Tier Entity 추가
    회원 경험치 정보 저장 table
    
    20230923 hoChan

---
+++ invest/src/main/java/com/jrjr/invest/rank/entity/Tier.java
@@ -0,0 +1,29 @@
+package com.jrjr.invest.rank.entity;
+
+import com.jrjr.invest.global.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tier")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tier extends BaseEntity {
+
+	@Id
+	private Long userSeq;
+
+	private Integer tier;
+
+	@Builder
+	public Tier(Long userSeq, Integer tier) {
+		this.userSeq = userSeq;
+		this.tier = tier;
+	}
+}
