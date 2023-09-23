c0902ec94a12f3410415cf7f215149ac80d77e4b
Hc-5514 <tbd05050@naver.com>
2023-09-23 22:25:49 +0900

    feat : RankServiceImpl (회원가입, 프로필 정보 수정 시, Redis 의 user 테이블 반영 로직 추가)
    회원가입 시, Redis 의 user 테이블에 정보 추가
    프로필 정보 수정 시, Redis 의 user 테이블에서 해당 회원의 닉네임, 프로필 이미지 수정
    
    20230923 hoChan

--- invest/src/main/java/com/jrjr/invest/rank/service/RankServiceImpl.java
+++ invest/src/main/java/com/jrjr/invest/rank/service/RankServiceImpl.java
@@ -1,5 +1,9 @@
 package com.jrjr.invest.rank.service;
 
+import java.util.Map;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.jrjr.invest.rank.dto.RedisUserDTO;
@@ -12,14 +16,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RankServiceImpl implements RankService {
 
+	private final RedisTemplate<String, RedisUserDTO> redisUserDtoRedisTemplate;
+
 	@Override
 	public void insertUserRankingInfo(RedisUserDTO redisUserDTO) {
 		log.info("회원 정보: {}", redisUserDTO.toString());
 
+		String hashKey = "user";
+		HashOperations<String, String, RedisUserDTO> redisUserDTOHashOperations = redisUserDtoRedisTemplate.opsForHash();
+		redisUserDTOHashOperations.put(hashKey, String.valueOf(redisUserDTO.getSeq()), redisUserDTO);
+
+		Map<String, RedisUserDTO> redisUserDTOMap = redisUserDTOHashOperations.entries(hashKey);
+		for (String seq : redisUserDTOMap.keySet()) {
+			log.info(redisUserDTOMap.get(seq).toString());
+		}
 	}
 
 	@Override
-	public void updateUserRankingInfo(RedisUserDTO redisUserDTO) {
-		log.info("회원 정보: {}", redisUserDTO.toString());
+	public void updateUserRankingInfo(RedisUserDTO inputRedisUserDTO) {
+		log.info("변경 요청 회원 정보: {}", inputRedisUserDTO.toString());
+
+		String hashKey = "user";
+		HashOperations<String, String, RedisUserDTO> redisUserDTOHashOperations = redisUserDtoRedisTemplate.opsForHash();
+		RedisUserDTO redisUserDTO = redisUserDTOHashOperations.get(hashKey, String.valueOf(inputRedisUserDTO.getSeq()));
+		log.info("변경 전 회원 정보: {}", redisUserDTO.toString());
+		redisUserDTO.setNickname(inputRedisUserDTO.getNickname());
+		redisUserDTO.setProfileImgSearchName(inputRedisUserDTO.getProfileImgSearchName());
+		redisUserDTOHashOperations.put(hashKey, String.valueOf(redisUserDTO.getSeq()), redisUserDTO);
+		log.info("변경 후 회원 정보: {}", redisUserDTO);
+
+		Map<String, RedisUserDTO> redisUserDTOMap = redisUserDTOHashOperations.entries(hashKey);
+		for (String seq : redisUserDTOMap.keySet()) {
+			log.info(redisUserDTOMap.get(seq).toString());
+		}
 	}
 }
