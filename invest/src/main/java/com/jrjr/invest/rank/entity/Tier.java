16a21198f790d3d948e2ae2060f166080b479757
Hc-5514 <tbd05050@naver.com>
2023-09-23 16:17:53 +0900

    feat : Tier Entity (column 옵션 추가)
    tier column null 미허용
    
    20230923 hoChan

--- invest/src/main/java/com/jrjr/invest/rank/entity/Tier.java
+++ invest/src/main/java/com/jrjr/invest/rank/entity/Tier.java
@@ -2,6 +2,7 @@ package com.jrjr.invest.rank.entity;
 
 import com.jrjr.invest.global.entity.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -19,6 +20,7 @@ public class Tier extends BaseEntity {
 	@Id
 	private Long userSeq;
 
+	@Column(nullable = false)
 	private Integer tier;
 
 	@Builder
