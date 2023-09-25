1f2589e93cc3f577509e08168bb14ffc3164ff38
YeahLim <yeahleem2@gmail.com>
2023-09-25 10:37:19 +0900

    feat : controller 추가 20230925 yeLim

--- invest/src/main/java/com/jrjr/invest/group/controller/GroupController.java
+++ invest/src/main/java/com/jrjr/invest/group/controller/GroupController.java
@@ -1,5 +1,9 @@
 package com.jrjr.invest.group.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jrjr.invest.group.dto.SimulationDTO;
+import com.jrjr.invest.group.dto.UserDTO;
 import com.jrjr.invest.group.service.GroupService;
 import com.jrjr.invest.simulation.entity.Simulation;
 
@@ -28,10 +33,12 @@ public class GroupController {
 		// db 생성
 
 		// 방장 redis 생성
+		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 
 	@GetMapping("/")
 	ResponseEntity<?> searchUsers(@RequestParam String keyword) {
-		groupService.searchUsers(keyword);
+		List<UserDTO> users = groupService.searchUsers(keyword);
+		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 }
