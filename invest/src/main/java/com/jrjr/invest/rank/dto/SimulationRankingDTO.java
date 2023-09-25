f5f71fd70bcde2afcec34a97f046a22baaec6073
Hc-5514 <tbd05050@naver.com>
2023-09-26 02:29:48 +0900

    feat : SimulationRankingDTO 추가 (시뮬레이션 별 참가자 랭킹 정보)
    시뮬레이션 별 참가자 랭킹 정보
    
    20230926 hoChan

---
+++ invest/src/main/java/com/jrjr/invest/rank/dto/SimulationRankingDTO.java
@@ -0,0 +1,36 @@
+package com.jrjr.invest.rank.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@ToString
+public class SimulationRankingDTO {
+
+	private Long simulationSeq;
+
+	private Long userSeq;
+
+	private Integer previousRank;
+
+	private Integer currentRank;
+
+	private Long currentMoney;
+
+	private String nickname;
+
+	private String profileImgSearchName;
+
+	private Long totalMoney;
+
+	private Integer rate;
+}
