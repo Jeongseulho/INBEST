59627bef4212b1272167eadb6e28a05511530752
Hc-5514 <tbd05050@naver.com>
2023-09-25 14:46:16 +0900

    feat : UserRepository, UserRedisRepository 추가
    RedisTierRankDTO 기반 hash table 추가
    티어별 랭크 산정 로직 추가
    티어 분포도 정보 가져오는 로직 추가
    
    20230925 hoChan

--- invest/src/main/java/com/jrjr/invest/rank/repository/UserRepository.java
+++ invest/src/main/java/com/jrjr/invest/rank/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.jrjr.invest.rank.repository;
 import java.util.Map;
 import java.util.Set;
 
+import com.jrjr.invest.rank.dto.RedisTierRankDTO;
 import com.jrjr.invest.rank.dto.RedisUserDTO;
 
 public interface UserRepository {
@@ -15,9 +16,9 @@ public interface UserRepository {
 
 	Set<RedisUserDTO> getUserInfoSet(long start, long end);
 
-	void insertUserInfo(RedisUserDTO redisUserDTO);
+	void insertUserInfo(RedisUserDTO redisUserDto);
 
-	void updateUserProfileInfo(RedisUserDTO inputRedisUserDTO);
+	void updateUserProfileInfo(RedisUserDTO redisUserDto);
 
 	void deleteUserInfo(Long seq);
 
@@ -28,4 +29,8 @@ public interface UserRepository {
 	void updateUserRankingList();
 
 	RedisUserDTO getMyRankingInfo(Long seq);
+
+	void updateTierRankList();
+
+	RedisTierRankDTO getTierRankList();
 }
