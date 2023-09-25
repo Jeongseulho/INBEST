cd92db9426718fb0a01515a0488578b40a7d2872
Hc-5514 <tbd05050@naver.com>
2023-09-26 01:16:25 +0900

    style : RedisSimulationUserDTO 수정
    RedisSimulationUserDTO 수정
    
    20230926 hoChan

--- invest/src/main/java/com/jrjr/invest/simulation/dto/RedisSimulationUserDTO.java
+++ invest/src/main/java/com/jrjr/invest/simulation/dto/RedisSimulationUserDTO.java
@@ -1,8 +1,5 @@
 package com.jrjr.invest.simulation.dto;
 
-import com.jrjr.invest.simulation.entity.Simulation;
-import com.jrjr.invest.simulation.entity.User;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,21 +7,29 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@ToString
 public class RedisSimulationUserDTO {
 
 	@NotNull
 	private Long simulationSeq;
+
 	@NotNull
 	private Long userSeq;
+
 	private Long seedMoney;
+
 	private Long currentMoney;
+
 	private Integer previousRank;
+
 	private Integer currentRank;
+
 	private Boolean isExited;
 }
