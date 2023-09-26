bd9f46237f5d0ad6732e4ddb73935c47bfe69258
Hc-5514 <tbd05050@naver.com>
2023-09-26 18:14:43 +0900

    feat : SimulationUserRankServiceImpl, SimulationUserRankRedisRepository (시뮬레이션의 참가자 랭킹 정보 계산 로직 추가)
    시뮬레이션의 참가자 랭킹 정보 계산 로직 추가 (테스트 x)
    
    20230926 hoChan

--- invest/src/main/java/com/jrjr/invest/rank/service/SimulationUserRankServiceImpl.java
+++ invest/src/main/java/com/jrjr/invest/rank/service/SimulationUserRankServiceImpl.java
@@ -19,16 +19,7 @@ public class SimulationUserRankServiceImpl implements SimulationUserRankService 
 
 	@Override
 	public void updateSimulationUserRanking(Long simulationSeq) {
-		// 시뮬레이션 별 랭킹 정보 업데이트
-		// 1. hashKey: simulationSeq 로 Map 형태의 simulation_user 를 가져온다.
-		// 2. for-each 로 탐색
-		// 2-1. 탈주 했다면, 현재 랭킹을 (* (-1)) 해서 totalMoney 에 넣고 sorted-set에 넣는다. rate 계산 x
-		// 2-1-1. 탈주하면, 탈주 인원 세려서 순위를 갱신해줘야 한다. 갱신 후 이 메서드 호출.
-		// 2-2. 탈주 안 했다면, totalMoney 를 계산한다.
-		// 2-2. 탑3 주식을 계산한다.
-		// 2-2. rate 를 계산한다.
-		// 2-3.
-		//
+		simulationUserRankRedisRepository.updateSimulationUserRankingList(simulationSeq);
 	}
 
 	@Override
