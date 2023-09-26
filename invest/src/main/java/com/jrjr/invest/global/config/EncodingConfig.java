3149da1b1b571c894dae2e6e3f6538126ba415b1
YeahLim <yeahleem2@gmail.com>
2023-09-26 16:47:29 +0900

    feat : 인코딩 필터 추가
    20230926 SuHyun

---
+++ invest/src/main/java/com/jrjr/invest/global/config/EncodingConfig.java
@@ -0,0 +1,16 @@
+package com.jrjr.invest.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class EncodingConfig {
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
+	}
+}
