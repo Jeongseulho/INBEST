01386811c6634ddb652f865083571fedbc0d2bdd
Hc-5514 <tbd05050@naver.com>
2023-09-27 02:44:35 +0900

    feat : RedisSimulationDTO 추가
    종료된 시뮬레이션 랭킹 산정을 위한 Dto
    
    20230927 hoChan

---
+++ invest/src/main/java/com/jrjr/invest/rank/dto/RedisSimulationDTO.java
@@ -0,0 +1,32 @@
+package com.jrjr.invest.rank.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@ToString
+public class RedisSimulationDTO {
+
+	private Long simulationSeq;
+
+	private String title;
+
+	private Integer previousRank;
+
+	private Integer currentRank;
+
+	private Integer period;
+
+	private Integer memberNum;
+
+	private Integer revenuRate;
+}
