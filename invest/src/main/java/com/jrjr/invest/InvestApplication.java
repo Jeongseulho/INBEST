b38958c0b46093f2264ad88794f1d823d1b88ddb
YeahLim <yeahleem2@gmail.com>
2023-09-26 16:37:08 +0900

    feat : 한글 검색 utf 설정 20230926 yeLim

--- invest/src/main/java/com/jrjr/invest/InvestApplication.java
+++ invest/src/main/java/com/jrjr/invest/InvestApplication.java
@@ -3,6 +3,8 @@ package com.jrjr.invest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,15 @@ public class InvestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestApplication.class, args);
+	}
+
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new
+			CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
 	}
 
 }
