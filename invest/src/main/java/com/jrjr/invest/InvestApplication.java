e2b2eb5aa96417db5033677f676f4c8cf379615d
Hc-5514 <tbd05050@naver.com>
2023-09-26 21:46:44 +0900

    feat : InvestApplication (@EnableJpaAuditing 추가)
    @EnableJpaAuditing 추가
    
    20230926 hoChan

--- invest/src/main/java/com/jrjr/invest/InvestApplication.java
+++ invest/src/main/java/com/jrjr/invest/InvestApplication.java
@@ -4,13 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ServletComponentScan
+@EnableJpaAuditing
 public class InvestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvestApplication.class, args);
 	}
+
 }
