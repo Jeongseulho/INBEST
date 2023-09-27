efebcdd8ead92283aecbcb3ed03de192635d7a0a
Hc-5514 <tbd05050@naver.com>
2023-09-27 10:51:04 +0900

    feat : SimulationRepository (종료된 시뮬레이션 정보를 리스트로 반환 (내림차순) )
    findByFinishedDateIsNotNullOrderByRevenuRateDesc() 추가
    
    20230927 hoChan

--- invest/src/main/java/com/jrjr/invest/simulation/repository/SimulationRepository.java
+++ invest/src/main/java/com/jrjr/invest/simulation/repository/SimulationRepository.java
@@ -1,5 +1,7 @@
 package com.jrjr.invest.simulation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.jrjr.invest.simulation.entity.Simulation;
 @Repository
 public interface SimulationRepository extends JpaRepository<Simulation, Long> {
 	Simulation findBySeq(Long seq);
+
+	List<Simulation> findByFinishedDateIsNotNullOrderByRevenuRateDesc();
 }
