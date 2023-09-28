b3ab45238c7c69dd696c926b8c63b2698b70b38d
Hc-5514 <tbd05050@naver.com>
2023-09-28 16:22:05 +0900

    docs : SimulationRankService, SimulationRankServiceImpl (메서드명 수정, 주석 추가)
    메서드명 수정, 주석 추가
    
    20230928 hoChan

--- invest/src/main/java/com/jrjr/invest/rank/service/SimulationRankService.java
+++ invest/src/main/java/com/jrjr/invest/rank/service/SimulationRankService.java
@@ -9,15 +9,18 @@ import com.jrjr.invest.rank.dto.SimulationRankingDTO;
 
 public interface SimulationRankService {
 
-	void updateSimulationUserRanking(Long simulationSeq);
+	void updateSimulationUserRankingInfo(Long simulationSeq); // 시뮬레이션 별 참가자 랭킹 정보 산정
 
+	// 시뮬레이션 별 전체 참가자 랭킹 정보 불러오기
 	Set<RedisSimulationUserRankingDTO> getSimulationUserRankingInfo(Long simulationSeq, Long start, Long end);
 
+	// 시뮬레이션 별 내 랭킹 정보 불러오기
 	RedisSimulationUserRankingDTO getSimulationUserRankingInfo(Long simulationSeq, Long userSeq);
 
-	List<SimulationRankingDTO> getSimulationRankingInfo();
+	List<SimulationRankingDTO> getSimulationRankingInfo(); // 전체 시뮬레이션 랭킹 정보 불러오기 (종료된 시뮬레이션)
 
+	// 시뮬레이션 별 BEST PICK 구하기 (수익률, 손실율, 산업군)
 	Map<String, List<Object>> getSimulationStockRankingInfo(Long simulationSeq);
 
-	Integer getSimulationAvgTierInfo(Long simulationSeq);
+	Integer getSimulationAvgTierInfo(Long simulationSeq); // 시뮬레이션의 평균 티어 정보 가져오기
 }
