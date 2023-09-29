4ad02fb7b4e72e5cb1d750345a268d1d0839014f
SuHyun Kim <khnemu@naver.com>
2023-09-29 20:16:26 +0900

    feat : 친구기능 추가
    20230929 SuHyun

---
+++ spring/src/main/java/com/jrjr/inbest/user/repository/FriendRepository.java
@@ -0,0 +1,17 @@
+package com.jrjr.inbest.user.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jrjr.inbest.user.entity.Friend;
+import com.jrjr.inbest.user.entity.User;
+
+@Repository
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+	Optional<List<Friend>> findAllByFollowedSeq(Long followedSeq);
+	Optional<List<Friend>> findAllByFollowingSeq(Long followingSeq);
+	Optional<Friend> findByFollowingSeqAndFollowedSeq(Long followingSeq,Long followedSeq);
+}
