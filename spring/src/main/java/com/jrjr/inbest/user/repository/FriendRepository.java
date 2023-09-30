beb0a8cfe891f14554961090e7545c53bb38dd3a
Hc-5514 <tbd05050@naver.com>
2023-09-30 21:54:04 +0900

    feat : FriendRepository (팔로잉, 팔로워 조회 메서드 추가)
    팔로잉, 팔로워 조회 메서드 추가
    
    20230930 hoChan

--- spring/src/main/java/com/jrjr/inbest/user/repository/FriendRepository.java
+++ spring/src/main/java/com/jrjr/inbest/user/repository/FriendRepository.java
@@ -4,14 +4,24 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.jrjr.inbest.user.entity.Friend;
-import com.jrjr.inbest.user.entity.User;
 
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
+
 	Optional<List<Friend>> findAllByFollowedSeq(Long followedSeq);
+
 	Optional<List<Friend>> findAllByFollowingSeq(Long followingSeq);
-	Optional<Friend> findByFollowingSeqAndFollowedSeq(Long followingSeq,Long followedSeq);
+
+	Optional<Friend> findByFollowingSeqAndFollowedSeq(Long followingSeq, Long followedSeq);
+
+	@Query("SELECT COUNT(f.followingSeq) FROM Friend f WHERE f.followingSeq = :userSeq")
+	Optional<Integer> getFollowingNum(@Param("userSeq") Long userSeq);
+
+	@Query("SELECT COUNT(f.followedSeq) FROM Friend f WHERE f.followedSeq = :userSeq")
+	Optional<Integer> getFollowerNum(@Param("userSeq") Long userSeq);
 }
