25afb5e4a21702b610556bf9b96390be5460d425
Hc-5514 <tbd05050@naver.com>
2023-10-01 17:17:39 +0900

    feat : TierByDateDTO 수정
    TierByDateDTO 수정
    
    20231001 hoChan

--- spring/src/main/java/com/jrjr/inbest/user/dto/TierByDateDTO.java
+++ spring/src/main/java/com/jrjr/inbest/user/dto/TierByDateDTO.java
@@ -1,5 +1,8 @@
 package com.jrjr.inbest.user.dto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -17,9 +20,14 @@ import lombok.ToString;
 @ToString
 public class TierByDateDTO {
 
-	@Schema(description = "날짜(YYYY-MM-DD)")
+	@Schema(description = "날짜(yyyy-MM-dd)")
 	String date;
 
 	@Schema(description = "티어 점수")
-	Integer tier;
+	Long tier;
+
+	public TierByDateDTO(LocalDateTime date, Long tier) {
+		this.date = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		this.tier = tier;
+	}
 }
