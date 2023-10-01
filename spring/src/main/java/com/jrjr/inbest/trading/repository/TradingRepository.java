d11e54ee53821fb9328d149945a486f382b6243a
Hc-5514 <tbd05050@naver.com>
2023-10-01 13:42:56 +0900

    feat : TradingRepository 추가 (calculatePurchaseAmountByIndustry 작성)
    1. trading table 에서 모든 구매 거래 내역 조회 후 구매 가격 합산
    2. financialdata_company 에서 stock_type 과 stock_code 를 이용해 산업군 이름 조회
    해당 결과를 IndustryDTO 에 넣는 jpql 추가
    
    20231001 hoChan

---
+++ spring/src/main/java/com/jrjr/inbest/trading/repository/TradingRepository.java
@@ -0,0 +1,27 @@
+package com.jrjr.inbest.trading.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.jrjr.inbest.trading.entity.Trading;
+import com.jrjr.inbest.user.dto.IndustryDTO;
+
+@Repository
+public interface TradingRepository extends CrudRepository<Trading, Long> {
+
+	@Query(
+		"SELECT NEW com.jrjr.inbest.user.dto.IndustryDTO(fc.companyIndustry, sum(t.price * t.amount)) "
+			+ "FROM Trading t "
+			+ "         JOIN FinancialdataCompany fc "
+			+ "              ON t.stockType = fc.companyStockType "
+			+ "                  AND t.stockCode = fc.companyStockCode "
+			+ "WHERE t.userSeq = :userSeq "
+			+ "  AND t.tradingType = 1 "
+			+ "  AND t.conclusionType = 1 "
+			+ "GROUP BY fc.companyIndustry")
+	List<IndustryDTO> calculatePurchaseAmountByIndustry(@Param("userSeq") Long userSeq);
+}
