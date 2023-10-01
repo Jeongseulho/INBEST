abf5df1abbee9dcf4060229a1e97cbe41a933e0e
Hc-5514 <tbd05050@naver.com>
2023-10-01 21:10:35 +0900

    feat : TradingRepository (시뮬레이션에서 자주 투자한 산업군 n가지 구하는 메서드 추가)
    시뮬레이션에서 자주 투자한 산업군 n가지 구하는 메서드 추가
    
    20231001 hoChan

--- spring/src/main/java/com/jrjr/inbest/trading/repository/TradingRepository.java
+++ spring/src/main/java/com/jrjr/inbest/trading/repository/TradingRepository.java
@@ -2,6 +2,7 @@ package com.jrjr.inbest.trading.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -24,4 +25,18 @@ public interface TradingRepository extends CrudRepository<Trading, Long> {
 			+ "  AND t.conclusionType = 1 "
 			+ "GROUP BY fc.companyIndustry")
 	List<IndustryDTO> calculatePurchaseAmountByIndustry(@Param("userSeq") Long userSeq);
+
+	@Query("SELECT fc.companyIndustry "
+		+ "FROM Trading t "
+		+ "         JOIN FinancialdataCompany fc "
+		+ "              ON t.stockType = fc.companyStockType "
+		+ "                  AND t.stockCode = fc.companyStockCode "
+		+ "WHERE t.userSeq = :userSeq "
+		+ "  AND t.simulationSeq = :simulationSeq "
+		+ "  AND t.tradingType = 1 "
+		+ "  AND t.conclusionType = 1 "
+		+ "GROUP BY fc.companyIndustry "
+		+ "ORDER BY sum(t.price * t.amount) DESC")
+	List<String> getTopNIndustry(@Param("simulationSeq") Long simulation_seq, @Param("userSeq") Long user_seq,
+		Pageable pageable);
 }
