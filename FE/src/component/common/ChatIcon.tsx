6f009c427d6cd9884a518b85eca8b07de3a0c097
Jeong seulho <wjdtmfgh@gmail.com>
2023-10-04 06:56:51 +0900

    feat : chat UI
    20231004 seulHo

--- FE/src/component/common/ChatIcon.tsx
+++ FE/src/component/common/ChatIcon.tsx
@@ -32,8 +32,60 @@ const ChatIcon = () => {
             leaveFrom="opacity-100 translate-y-0"
             leaveTo="opacity-0 translate-y-1"
           >
-            <Popover.Panel className="absolute bottom-96 border-2">
-              <div>안녕</div>
+            <Popover.Panel className="absolute bottom-24 border-2 right-0 w-96 h-[500px] rounded-md bg-slate-100 ">
+              <div className="h-full flex flex-col">
+                <div className="w-full bg-mainDark h-12 pt-2 text-white flex justify-center items-center shadow-md">
+                  <div className="text-green-100 font-bold text-lg tracking-wide">title</div>
+                </div>
+                <div className="overflow-y-scroll flex-col">
+                  <div className="clearfix flex justify-start">
+                    <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      this is a basic mobile chat layout, build with tailwind css
+                    </div>
+                  </div>
+                  <div className="clearfix flex justify-start">
+                    <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      this is a basic mobile chat layout, build with tailwind css
+                    </div>
+                  </div>
+                  <div className="clearfix flex justify-start">
+                    <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      this is a basic mobile chat layout, build with tailwind css
+                    </div>
+                  </div>
+                  <div className="clearfix flex justify-start">
+                    <div className="bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      this is a basic mobile chat layout, build with tailwind css
+                    </div>
+                  </div>
+                  <div className="clearfix flex justify-end">
+                    <div className="bg-green-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      check my twitter to see when it will be released.
+                    </div>
+                  </div>
+                  <div className="clearfix flex justify-end">
+                    <div className="bg-green-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix text-sm">
+                      check my twitter to see when it will be released.
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full flex justify-between bg-green-100">
+                  <textarea
+                    className="flex-grow m-2 py-3 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none text-sm items-center"
+                    rows={1}
+                    placeholder="Message..."
+                    style={{ outline: "none" }}
+                  ></textarea>
+                  <button
+                    className="m-2"
+                    style={{
+                      outline: "none",
+                    }}
+                  >
+                    {/* Send 버튼 아이콘 */}
+                  </button>
+                </div>
+              </div>
             </Popover.Panel>
           </Transition>
         </>
