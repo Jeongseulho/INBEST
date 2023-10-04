4ff370f0548f6faef8a4031b6db96e443a8c7660
YeahLim <yeahleem2@gmail.com>
2023-10-05 00:10:12 +0900

    feat : rabbitmq 변수 안전하게 이동 20231005 yeLim

--- chat/src/main/java/com/jrjr/chat/controller/RabbitController.java
+++ chat/src/main/java/com/jrjr/chat/controller/RabbitController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -16,12 +17,18 @@ public class RabbitController {
     private final RabbitTemplate rabbitTemplate;
     private final ChatService chatService;
 
+    @Value("${custom.rabbitmq.exchange}")
+    private String EXCHANGE_NAME;
+
+    @Value("${custom.rabbitmq.routing-key.chat}")
+    private String CHAT_ROUTING_KEY;
+
     @RabbitListener(queues = "realtime-queue")
     public void receiveRealTimeMessage(ChatDTO chatDTO) {
         log.info("RealTime Service에서 온 메세지: {}", chatDTO.toString());
         chatDTO = chatService.setMessage(chatDTO);
         chatDTO.setDateTime();
         chatDTO.setProfileImgSearchName(chatService.setProfileImage(chatDTO.getUserSeq()));
-        rabbitTemplate.convertAndSend("realtime_direct", "chat", chatDTO);
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, CHAT_ROUTING_KEY, chatDTO);
     }
 }
