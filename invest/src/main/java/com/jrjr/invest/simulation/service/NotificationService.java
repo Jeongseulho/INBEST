4643077b105512c942ee6de8798105ec90fd6cfa
YeahLim <yeahleem2@gmail.com>
2023-10-04 17:35:46 +0900

    feat ; rabbitmq 변수 application.yml로 이동 20231004 yeLim

--- invest/src/main/java/com/jrjr/invest/simulation/service/NotificationService.java
+++ invest/src/main/java/com/jrjr/invest/simulation/service/NotificationService.java
@@ -1,70 +1,77 @@
 package com.jrjr.invest.simulation.service;
 
-
-import com.jrjr.invest.simulation.document.Notification;
-import com.jrjr.invest.simulation.dto.notification.MessageDTO;
-import com.jrjr.invest.simulation.repository.NotificationRepository;
-import com.jrjr.invest.simulation.repository.SimulationRepository;
-import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.jrjr.invest.simulation.document.Notification;
+import com.jrjr.invest.simulation.repository.NotificationRepository;
+import com.jrjr.invest.simulation.repository.SimulationRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Service
 public class NotificationService {
 
-    private final RabbitTemplate rabbitTemplate;
-    private final SimulationRepository simulationRepository;
-    private final NotificationRepository notificationRepository;
+	@Value("${custom.rabbitmq.exchange}")
+	private String EXCHANGE_NAME;
 
-    // 초대 요청 알림 보내기
-    @Transactional
-    public void inviteUser(Long simulationSeq, String ownerNickname, Long userSeq) {
-        log.info("[초대 요청 알림 보내기]");
+	@Value("${custom.rabbitmq.routing-key.invest}")
+	private String INVEST_ROUTING_KEY;
 
-        String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
-        Notification notification = Notification.builder()
-                .simulationSeq(simulationSeq)
-                .userSeq(userSeq)
-                .build();
-        notification.setInvititionMessage(simulationTitle, ownerNickname);
-        notificationRepository.save(notification);
+	private final RabbitTemplate rabbitTemplate;
+	private final SimulationRepository simulationRepository;
+	private final NotificationRepository notificationRepository;
 
-        rabbitTemplate.convertAndSend("realtime_direct", "invest", notification.toNotificationDTO());
-    }
+	// 초대 요청 알림 보내기
+	@Transactional
+	public void inviteUser(Long simulationSeq, String ownerNickname, Long userSeq) {
+		log.info("[초대 요청 알림 보내기]");
 
-    // 시뮬레이션 시작 알림 메세지 보내기
-    @Transactional
-    public void sendStartNotification(Long simulationSeq, Long userSeq) {
-        log.info("[시뮬레이션 시작 알림 메세지 보내기]");
+		String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
+		Notification notification = Notification.builder()
+			.simulationSeq(simulationSeq)
+			.userSeq(userSeq)
+			.build();
+		notification.setInvititionMessage(simulationTitle, ownerNickname);
+		notificationRepository.save(notification);
 
-        String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
-        Notification notification = Notification.builder()
-                .simulationSeq(simulationSeq)
-                .userSeq(userSeq)
-                .build();
-        notification.setStartMessage(simulationTitle);
-        notificationRepository.save(notification);
+		rabbitTemplate.convertAndSend(EXCHANGE_NAME, INVEST_ROUTING_KEY, notification.toNotificationDTO());
+	}
 
-        rabbitTemplate.convertAndSend("realtime_direct", "invest", notification.toNotificationDTO());
-    }
+	// 시뮬레이션 시작 알림 메세지 보내기
+	@Transactional
+	public void sendStartNotification(Long simulationSeq, Long userSeq) {
+		log.info("[시뮬레이션 시작 알림 메세지 보내기]");
 
-    @Transactional
-    public void sendFinishNotification(Long simulationSeq, Long userSeq) {
-        log.info("[시뮬레이션 종료 알림 메세지 보내기]");
+		String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
+		Notification notification = Notification.builder()
+			.simulationSeq(simulationSeq)
+			.userSeq(userSeq)
+			.build();
+		notification.setStartMessage(simulationTitle);
+		notificationRepository.save(notification);
 
-        String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
-        Notification notification = Notification.builder()
-                .simulationSeq(simulationSeq)
-                .userSeq(userSeq)
-                .build();
-        notification.setFinishMessage(simulationTitle);
-        notificationRepository.save(notification);
+		rabbitTemplate.convertAndSend(EXCHANGE_NAME, INVEST_ROUTING_KEY, notification.toNotificationDTO());
+	}
 
-        rabbitTemplate.convertAndSend("realtime_direct", "invest", notification.toNotificationDTO());
-    }
+	@Transactional
+	public void sendFinishNotification(Long simulationSeq, Long userSeq) {
+		log.info("[시뮬레이션 종료 알림 메세지 보내기]");
+
+		String simulationTitle = simulationRepository.findBySeq(simulationSeq).getTitle();
+		Notification notification = Notification.builder()
+			.simulationSeq(simulationSeq)
+			.userSeq(userSeq)
+			.build();
+		notification.setFinishMessage(simulationTitle);
+		notificationRepository.save(notification);
+
+		rabbitTemplate.convertAndSend(EXCHANGE_NAME, INVEST_ROUTING_KEY, notification.toNotificationDTO());
+	}
 
 }
