bf7521eded4d3fa6a157dab940e16a727367a78e
SuHyun Kim <khnemu@naver.com>
2023-10-05 12:10:55 +0900

    feat: 상수를 사용하여 숫자 직접 비교 변경
    20231004 SuHyun

--- trading/src/main/java/com/jrjr/inbest/trading/document/Notification.java
+++ trading/src/main/java/com/jrjr/inbest/trading/document/Notification.java
@@ -1,5 +1,8 @@
 package com.jrjr.inbest.trading.document;
 
+import com.jrjr.inbest.trading.constant.StockType;
+import com.jrjr.inbest.trading.constant.TradingResultType;
+import com.jrjr.inbest.trading.constant.TradingType;
 import com.jrjr.inbest.trading.dto.NotificationDTO;
 import com.jrjr.inbest.trading.dto.TradingDTO;
 import lombok.Builder;
@@ -43,7 +46,7 @@ public class Notification {
     }
 
     public void setApplyTradingMessage(String simulationTitle, TradingDTO tradingDTO) {
-        String tradingType = tradingDTO.getTradingType() == 0 ? "매도" : "매수";
+        String tradingType = tradingDTO.getTradingType() == TradingType.SELL ? "매도" : "매수";
         this.message = simulationTitle+ "에서 " + tradingDTO.getStockName() + " " + tradingDTO.getAmount() + "주 " +
             tradingDTO.getPrice() + "원(KRW)에 " + tradingType + " 신청이 완료되었습니다.";
     }
@@ -51,20 +54,20 @@ public class Notification {
     public void setTradingMessage(String simulationTitle, TradingDTO tradingDTO) {
 
         // 매매 타입
-        String tradingType = tradingDTO.getTradingType() == 0 ? "매도" : "매수";
+        String tradingType = tradingDTO.getTradingType() == TradingType.SELL ? "매도" : "매수";
 
         // 성공 여부
         String conclusionType = "미체결";
-        if (tradingDTO.getConclusionType() == 1) {
+        if (tradingDTO.getConclusionType() == TradingResultType.SUCCESS) {
             this.title = "주식 체결 성공 알림";
             conclusionType = "성공";
-        } else if (tradingDTO.getConclusionType() == 2) {
+        } else if (tradingDTO.getConclusionType() == TradingResultType.SUCCESS) {
             this.title = "주식 체결 실패 알림";
             conclusionType = "실패";
         }
 
         // 가상화페인 경우
-        String amountType = tradingDTO.getStockType() == 2 ? "개" : "주";
+        String amountType = tradingDTO.getStockType() == StockType.CRYPTO_MONEY ? "개" : "주";
 
         // 가격 쉼표 처리
         DecimalFormat decFormat = new DecimalFormat("###,###");
