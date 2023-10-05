76f87cb591b63b8fe888038d4a2d2a408d07f87f
Jeong seulho <wjdtmfgh@gmail.com>
2023-10-05 10:11:56 +0900

    feat : private route
    20231005 seulHo

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -19,6 +19,8 @@ import "react-loading-skeleton/dist/skeleton.css";
 import PersonalRanking from "./component/ranking/organisms/PersonalRanking";
 import PersonalRankingSearch from "./component/ranking/organisms/PersonalRankingSearch";
 import GroupRanking from "./component/ranking/organisms/GroupRanking";
+import PrivateRoute from "./component/common/PrivateRoute";
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -38,21 +40,42 @@ function App() {
           <AnimatePresence mode="wait">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="login" element={<LoginSignup />} />
-              <Route path="/invest" element={<InvestMain />} />
-              <Route path="login/oauth2/code/kakao" element={<Oauth />} />
-              <Route path="login/oauth2/code/naver" element={<Oauth />} />
-              <Route path="community" element={<Community />}>
-                <Route index element={<CommunityList />} />
-                <Route path="create" element={<CommunityCreate />} />
-                <Route path="detail" element={<CommunityDetail />} />
+
+              <Route element={<PrivateRoute requireAuth={false} />}>
+                <Route path="login" element={<LoginSignup />} />
               </Route>
-              <Route path="/invest/:simulationSeq" element={<Investing />} />
-              <Route path="ranking" element={<Ranking />}>
-                <Route index element={<PersonalRanking />} />
-                <Route path="group" element={<GroupRanking />} />
-                <Route path="group/:seq" element={<GroupRanking />} />
-                <Route path="search/:nickname" element={<PersonalRankingSearch />} />
+
+              <Route element={<PrivateRoute requireAuth={true} />}>
+                <Route path="/invest" element={<InvestMain />} />
+              </Route>
+
+              <Route element={<PrivateRoute requireAuth={false} />}>
+                <Route path="login/oauth2/code/kakao" element={<Oauth />} />
+              </Route>
+
+              <Route element={<PrivateRoute requireAuth={false} />}>
+                <Route path="login/oauth2/code/naver" element={<Oauth />} />
+              </Route>
+
+              <Route element={<PrivateRoute requireAuth={true} />}>
+                <Route path="community" element={<Community />}>
+                  <Route index element={<CommunityList />} />
+                  <Route path="create" element={<CommunityCreate />} />
+                  <Route path="detail" element={<CommunityDetail />} />
+                </Route>
+              </Route>
+
+              <Route element={<PrivateRoute requireAuth={true} />}>
+                <Route path="/invest/:simulationSeq" element={<Investing />} />
+              </Route>
+
+              <Route element={<PrivateRoute requireAuth={true} />}>
+                <Route path="ranking" element={<Ranking />}>
+                  <Route index element={<PersonalRanking />} />
+                  <Route path="group" element={<GroupRanking />} />
+                  <Route path="group/:seq" element={<GroupRanking />} />
+                  <Route path="search/:nickname" element={<PersonalRankingSearch />} />
+                </Route>
               </Route>
             </Routes>
           </AnimatePresence>
