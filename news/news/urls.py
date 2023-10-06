37e5b9f075a647431025a63f8d897b9b9f7d4e8e
장진욱 <>
2023-10-06 11:06:53 +0900

    etc : brench mrege 231006 jinWook

--- news/news/urls.py
+++ news/news/urls.py
@@ -7,7 +7,6 @@ from . import views
 #     path("industry/<str:category>/", views.IndustryByNewsList),  #산업별 뉴스 목록
 #     path("company/<str:company_code>/", views.CompanyByNewsList),  # 기업별 뉴스
 # ]
-
 urlpatterns = [
     path("", views.MainNewsList),  # 메인 뉴스 목록
     path("breakingnews/", views.BreakingNewsList),  # 뉴스 속보 목록
